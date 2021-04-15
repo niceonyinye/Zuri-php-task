4626295b0fafb351f641ab2f38ed6d235f6c28a7
Judith <68328667+niceonyinye@users.noreply.github.com>
2021-04-15 07:26:41 +0100

    Add files via upload

---
+++ Php form/index.php
@@ -0,0 +1,21 @@
+
+
+<?php  include_once('lib/header.php');  require_once('functions/alert.php'); ?>
+
+    <p>
+        <?php  print_alert(); ?>
+    </p>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4"> <strong> Welcome to Acadia Hospital</strong></h1>
+        <p class="lead"><strong>Empowering People to Improve Their Lives.</strong>  </p>
+        <p class="lead"> <strong>Changing the way you receive healthcare.</strong> </p>
+        <p>
+            <a class="btn btn-bg btn-outline-secondary" href="login.php"><strong> Login </strong>  </a>
+            <a class="btn btn-bg btn-outline-primary" href="register.php">  <strong> Register </strong> </a>            
+        </p>
+    </div>
+  
+    
+<?php include_once('lib/footer.php'); ?>
+
